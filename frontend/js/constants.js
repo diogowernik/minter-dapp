b265e9fea5fd1d6fc4701771c0b09687820d1c5c
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2022-03-26 20:57:45 +0300

    updated project

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x82054Bdb5fa7c30918F3444f71eb712158c40a81"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
